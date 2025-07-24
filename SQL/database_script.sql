e7799ff83526b47495faa4b2d4c790be517c9719
aaarif796 <aaarif796@gmail.com>
2025-07-24 16:49:49 +0530

    table creation and er diagram done

--- SQL/database_script.sql
+++ SQL/database_script.sql
@@ -69,3 +69,53 @@ create table BookAuthor(
     foreign key (book_id) references Book(book_id) ON DELETE RESTRICT ON UPDATE CASCADE,
     foreign key (author_id) references Author(author_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+show tables;
+create table Category(
+	category_id int auto_increment primary key,
+    name varchar(30) not null,
+    description varchar(100)
+);
+create table BookCategory(
+	book_id int not null,
+	category_id int not null,
+    primary key(book_id, category_id),
+    foreign key (book_id) references Book(book_id) on delete restrict on update cascade,
+    foreign key (category_id) references Category(category_id) on delete restrict on update cascade
+);
+
+
+create table Members(
+	member_id int auto_increment primary key,
+    first_name varchar(15) not null,
+    last_name varchar(15),
+    email varchar(30) unique,
+    phone varchar(15),
+    member_type varchar(10),
+    registration_date datetime
+);
+
+create table Borrowing(
+	borrowing_id int auto_increment primary key,
+    member_id int,
+    book_id int,
+    borrow_date datetime,
+    due_date datetime,
+    return_date datetime,
+    late_fee int,
+    foreign key (member_id) references Members(member_id) ON UPDATE CASCADE,
+    foreign key (book_id) references Book(book_id) ON UPDATE CASCADE
+);
+
+create table Review(
+	review_id int auto_increment primary key,
+    member_id int,
+    book_id int,
+    rating smallint unsigned,
+    comments varchar(30),
+    review_data varchar(50),
+    foreign key (member_id) references Members(member_id) ON UPDATE CASCADE,
+    foreign key (book_id) references Book(book_id) ON UPDATE CASCADE
+);
+
+show tables;
