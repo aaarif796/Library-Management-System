b1d00f24e9a95b5fb85b2204aa42e6e607c06d97
aaarif796 <aaarif796@gmail.com>
2025-07-29 12:35:04 +0530

    Added all the tables validation

--- data_ingestion/data_validation.py
+++ data_ingestion/data_validation.py
@@ -52,6 +52,71 @@ class Library(BaseModel):
         return v
 
 
+class Author(BaseModel):
+    author_id: int
+    first_name: str
+    last_name: str
+    birth_date: date
+    nationality: str
+    biography: str
+
+
+class Borrowing(BaseModel):
+    borrowing_id: int
+    book_id: int
+    member_id: int
+    borrow_date: date
+    due_date: date
+    return_date: date
+    late_fee: date
+
+class Member(BaseModel):
+    member_id: int
+    first_name: str
+    last_name: str
+    email: str
+    phone: int
+    member_type: str
+    registration_date: date
+
+    @field_validator('email')
+    def validate_contact_email(cls, v: str) -> str:
+        pattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        if not re.match(pattern, v):
+            raise ValidationError("Invalid Email Address")
+        return v
+
+    @field_validator('phone')
+    def validate_phone_number(cls, v: str) -> str:
+        pattern = "^[+0-9]+[-\s]*[0-9]+$"
+        if not re.match(pattern, v) or len(v) < 10:
+            raise ValidationError("Invalid Phone number")
+        return v
+
+    @field_validator('member_type')
+    def validate_member_type(cls, v: str) -> str:
+        if v.lower() != "student" or v.lower() != "faculty":
+            raise ValidationError("Invalid Member type")
+        return v
+
+class Review(BaseModel):
+    review_id: int
+    book_id: int
+    member_id: int
+    rating: int
+    comment: str
+    review_date: date
+
+    @field_validator('rating')
+    def validate_rating(cls, rating: int) -> int:
+        if rating < 1 or rating > 5:
+            raise ValidationError("Rating should be between 1-5")
+        return rating
+
+class Category(BaseModel):
+    category_id: int
+    name: str
+    description: str
 
 
 
