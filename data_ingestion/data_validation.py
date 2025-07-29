b00b4f49bfa06c1bce8fe394486fceba77af94fb
aaarif796 <aaarif796@gmail.com>
2025-07-29 16:34:48 +0530

    Adding validation for all the attributes

--- data_ingestion/data_validation.py
+++ data_ingestion/data_validation.py
@@ -8,15 +8,58 @@ import json
 
 class Book(BaseModel):
     book_id: int
+    library_id: int
     title: str
-    author: str
     isbn: str
-    available_copies: int
     total_copies: int
-    publication: date
+    publication_date: date
+    available_copies: int
+
+    @field_validator('book_id')
+    def validate_book_id(cls, v: int) -> int:
+        """
+            This function is used to validate the book_id which should not be negative
+        :param v:
+        :return:
+        """
+        if v<1:
+            raise ValidationError("Book id cannot be negative")
+        return v
+
+    @field_validator('library_id')
+    def validate_library_id(cls, v: int) -> int:
+        """
+            This function is used to validate the library_id which check it should not be positive
+        :param v:
+        :return:
+        """
+        if v<1:
+            raise "Library_id should be positive"
+        return v
+
+    @field_validator('title')
+    def validate_title(cls, v:str) ->str:
+        """
+            In this function first trim the string,
+            split the string with the space
+            and add capitalization function before joining
+            after that join with the space
+        :param v:
+        :return:
+        """
+        trimmed = v.strip()
+        split_title = trimmed.split()
+        capitalized = [title.capitalize() for title in split_title]
+        concat_title = ' '.join(capitalized)
+        return concat_title
 
     @field_validator("isbn")
     def validate_isbn(cls, v:str) ->str:
+        """
+            Validationg the isbn where isbn will contain either 10 or 13 digits
+        :param v:
+        :return:
+        """
         cleaned = re.sub(r"[-\s]", "",v)
         if len(cleaned) not in (10,13):
             raise ValueError("ISBN must be 10 or 13 digits")
@@ -26,8 +69,24 @@ class Book(BaseModel):
 
     @field_validator("available_copies")
     def validate_available_copies(cls, v:int) ->int:
+        """
+            Validation available copies which should not be less than 0 or 0
+        :param v:
+        :return:
+        """
         if v <= 0:
             raise ValidationError("Available copies won't be less than 0")
+        return v
+
+    @field_validator("publication_date")
+    def validate_publication_date(cls, v: date) -> date:
+        """
+            Validation publication date which should not be more than present date
+        :param v:
+        :return:
+        """
+        if v > date.today():
+            raise ValidationError("It's not possible to give the future date")
         return v
 
 class Library(BaseModel):
@@ -37,15 +96,52 @@ class Library(BaseModel):
     contact_email: str
     phone_number = int
 
+    @field_validator('library_id')
+    def validate_library_id(cls, v: int) -> int:
+        """
+        Validating the library id which must be more than 0
+        :param v:
+        :return:
+        """
+        if v<1:
+            raise ValidationError("Primary id should not be 0 or less than it")
+        return v
+
+    @field_validator('Name')
+    def validate_name(cls, v:str) ->str:
+        """
+            In this function first trim the string,
+            split the string with the space
+            and add capitalization function before joining
+            after that join with the space
+        :param v:
+        :return:
+        """
+        trimmed = v.strip()
+        split_name = trimmed.split()
+        capitalized = [name.capitalize() for name in split_name]
+        library_name = ' '.join(capitalized)
+        return library_name
+
     @field_validator('contact_email')
     def validate_contact_email(cls, v: str) -> str:
+        """
+        Validating the email which must follow email pattern and it's should be in lowercase
+        :param v:
+        :return:
+        """
         pattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
         if not re.match(pattern, v):
             raise ValidationError("Invalid Email Address")
-        return v
+        return str.lower(v)
 
     @field_validator('phone_number')
     def validate_phone_number(cls, v:str) -> str:
+        """
+            Validating the phone number
+        :param v:
+        :return:
+        """
         pattern = "^[+0-9]+[-\s]*[0-9]+$"
         if not re.match(pattern, v) or len(v)<10:
             raise ValidationError("Invalid Phone number")
@@ -84,7 +180,7 @@ class Member(BaseModel):
         pattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
         if not re.match(pattern, v):
             raise ValidationError("Invalid Email Address")
-        return v
+        return str.lower(v)
 
     @field_validator('phone')
     def validate_phone_number(cls, v: str) -> str:
