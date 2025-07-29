826592631b55711b4d916369479ac288c7f0c03a
aaarif796 <aaarif796@gmail.com>
2025-07-29 10:49:02 +0530

    update data validate file

--- data_ingestion/data_validation.py
+++ data_ingestion/data_validation.py
@@ -1,9 +1,9 @@
-from xml.dom import VALIDATION_ERR
-
-from pydantic import BaseModel, EmailStr, Field, validator,ValidationError
+from pydantic import BaseModel, EmailStr, Field, field_validator,ValidationError, ValidationInfo
 import json
+from typing import List, Dict, Any
 from datetime import date
-
+import re
+import json
 
 # "book_id": 2,
 # "title": "Remember dream position",
@@ -20,32 +20,19 @@ class Book(BaseModel):
     available_copies: int
     total_copies: int
     publication: date
-#
-# class Book(BaseModel):
-#     name: str
-#     age: int
-#     grade: str
-#
-#     @validator('name'):
-#     def name_must_not_be_empty(cls,v):
-#         if not v.strip():
-#             raise ValueError("Name cannot be empty")
-#         return v
-#
-#     @validator('age')
-#     def age_must_be_positive(cls, v):
-#         if v<0:
-#             raise ValueError('Age must be positive')
-#         return v
-#
 
-with open('sample_data_of_book.json', 'r') as f:
-    data = json.load(f)
+    @field_validator("isbn")
+    def validate_isbn(cls, v:str) ->str:
+        cleaned = re.sub(r"[-\s]", "",v)
+        if len(cleaned) not in (10,13):
+            raise ValueError("ISBN must be 10 or 13 digits")
+        if not cleaned.isdigit():
+            raise ValueError("ISBN must contain number")
+        return cleaned
 
-for record in data:
-    try:
-        book = Book(**record)
-        print(f"Valid employee record:{book.title}")
-    except ValidationError as e:
-        print(f"Invalid employee record:{record['title']}")
-        print(f'Errors:{e.errors()}')
+    @field_validator("available_copies")
+    def validate_available_copies(cls, v:int) ->int:
+        if v <= 0:
+            raise ValidationError("Available copies won't be less than 0")
+        return v
+
