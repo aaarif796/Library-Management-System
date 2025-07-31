810e3bf67b26a5b1735b1283c74aa6637d91e85e
aaarif796 <aaarif796@gmail.com>
2025-07-31 12:02:29 +0530

    adding all the attributes in model.py files

--- data_ingestion/models.py
+++ data_ingestion/models.py
@@ -1,6 +1,6 @@
 import datetime
 import sqlalchemy as db
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, foreign
 
 engine = db.create_engine("mysql+pymysql://root:root@127.0.0.1:3306/LMS_ORM")
 Base = declarative_base()
@@ -25,26 +25,54 @@ class Book(Base):
 
 class Author(Base):
     __table_name__ = "Author"
-    
+    author_id = db.Column(db.Integer, priamry_key = True, autoincrement = True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    birth_date = db.Column(db.Date)
+    nationality = db.Column(db.String)
+    biography = db.Column(db.Text)
 
 class BookAuthor(Base):
     __table_name__ = "BookAuthor"
+
+class Category(Base):
+    __table_name = "Category"
+    category_id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    name = db.Column(db.String)
+    description = db.Column(db.Text)
 
 class BookCategory(Base):
     __table_name__ = "BookCategory"
 
 
-class Category(Base):
-    __table_name = "Category"
-
 class Borrowing(Base):
     __table_name__ = "Borrowing"
+    borrowing_id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    book_id = db.Column(db.Integer, db.ForeignKey('Book.book_id'))
+    member_id = db.Column(db.Integer, db.ForeignKey('Member.member_id'))
+    borrow_date = db.Column(db.Date)
+    due_date = db.Column(db.Date)
+    return_date = db.Column(db.Date)
+    late_fee = db.Column(db.Integer)
 
 class Member(Base):
     __table_name__ = "Member"
+    member_id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    email = db.Column(db.String)
+    phone = db.Column(db.String)
+    member_type = db.Column(db.String)
+    registration_date = db.Column(db.Date)
 
 class Review(Base):
     __table_name__ = "Review"
+    review_id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    book_id = db.Column(db.Integer, db.ForeignKey('Book.book_id'))
+    member_id = db.Column(db.Integer, db.ForeignKey('Member.member_id'))
+    rating = db.Column(db.Integer)
+    comment = db.Column(db.Text)
+    review_date = db.Column(db.Date)
 
 
 
