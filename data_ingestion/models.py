5c5fbd7c3cbd93394d2eea1982815c83f28bab04
aaarif796 <aaarif796@gmail.com>
2025-07-31 11:40:45 +0530

    Created book table

--- data_ingestion/models.py
+++ data_ingestion/models.py
@@ -8,10 +8,20 @@ Base = declarative_base()
 
 class Library(Base):
     __table_name__ = "Library"
-    id = db.Column(db.Integer, primary_key = True, autoincrement= True)
+    library_id = db.Column(db.Integer, primary_key = True, autoincrement= True)
     name = db.Column(db.String)
     campus_location = db.Column(db.String)
     email = db.Column(db.String)
+
+class Book(Base):
+    __table_name__ = "Book"
+    book_id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    library_id = db.Column(db.Integer, db.ForeignKey('Library.library_id'))
+    title = db.Column(db.String)
+    isbn = db.Column(db.String)
+    publication_date = db.Column(db.Date)
+    total_copies = db.Column(db.Integer)
+    available_copies = db.Column(db.Integer)
 
 
 
