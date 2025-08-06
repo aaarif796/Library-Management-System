229621f77c03f4bdfc08bc037c34e1f7fc0390e7
aaarif796 <aaarif796@gmail.com>
2025-08-06 12:24:22 +0530

    Connected api

--- django_api/lms_api/library/views.py
+++ django_api/lms_api/library/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from requests import Response
 from rest_framework.decorators import api_view
-
+from rest_framework.response import Response
 
 @api_view(['GET'])
 def health_check(request):
-    return Response({"status":"OK"})
+    return Response({"status": "OK"})
 
