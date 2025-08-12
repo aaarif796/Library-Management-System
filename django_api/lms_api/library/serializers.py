78a138cfbd2436c3496bce15fbbbcd78acfd3bdf
aaarif796 <aaarif796@gmail.com>
2025-08-12 12:57:17 +0530

    Completed validation

--- django_api/lms_api/library/serializers.py
+++ django_api/lms_api/library/serializers.py
@@ -1,6 +1,7 @@
-from django.core.validators import EmailValidator
+from django.core.validators import EmailValidator, RegexValidator
 from rest_framework import serializers
 from .models import Book, Library_Col, Author, Category, Member, Review, Borrowing
+from django.utils import timezone
 
 class LibrarySerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,8 +12,9 @@ class LibrarySerializer(serializers.ModelSerializer):
         return value.strip().title()
 
     def validate_contact_email(self, value):
+        value = value.strip().lower()
         EmailValidator()(value)
-        return value.lower()
+        return value
 
     def validate_campus_location(self, value):
         return value.strip().title()
@@ -23,13 +25,13 @@ class AuthorSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate_first_name(self, value):
-        return value.capatilize()
+        return value.capitalize()
 
     def validate_last_name(self, value):
-        return value.capatilize()
+        return value.capitalize()
 
     def validate_nationality(self, value):
-        return value.capatilize()
+        return value.capitalize()
 
 class BookSerializer(serializers.ModelSerializer):
     library_name = serializers.CharField(source="library.l_name", read_only = True)
@@ -38,16 +40,25 @@ class BookSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate_title(self, value):
-        return value.title()
+        return value.strip().title()
 
     def validate_isbn(self, value):
-        pass
+        isbn = value.replace("-", "").replace(" ","")
+        if len(isbn) not in [10,13]:
+            raise serializers.ValidationError("Must be 10 or 13")
+        if not isbn.isdigit():
+            raise serializers.ValidationError("Must be digits")
+        return isbn
 
     def validate_publication_date(self, value):
-        pass
+        if value > timezone.now().date():
+            raise serializers.ValidationError("Enter the correct publication date")
+        return value
 
     def validate_total_copies(self, value):
-        pass
+        if value<0:
+            raise serializers.ValidationError("Enter the correct total copies")
+        return value
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -56,7 +67,7 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate_name(self, value):
-        pass
+        return value.strip().title()
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -67,7 +78,9 @@ class ReviewSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate_rating(self, value):
-        pass
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Rating must be between 1 to 5")
+        return value
 
 
 class MemberSerializer(serializers.ModelSerializer):
@@ -76,22 +89,33 @@ class MemberSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate_first_name(self, value):
-        pass
+        return value.strip().capitalize()
 
     def validate_last_name(self, value):
-        pass
+        return value.strip().capitalize() if value else value
 
     def validate_email(self, value):
-        pass
+        value = value.strip().lower()
+        EmailValidator()(value)
+        return value.strip()
 
     def validate_phone(self, value):
-        pass
+        phone = value.strip().replace(" ", "").replace("-", "")
+        if not phone.startswith("+"):
+            phone = "+91" + phone  # default country code
+        return phone
 
     def validate_member_type(self, value):
-        pass
+        contains = ('Student', 'Faculty','Guest')
+        if value.strip().capitalize() in contains:
+            return value
+        else:
+            raise serializers.ValidationError("It must be any of these: ('Student', 'Faculty','Guest')")
 
     def validate_registration_date(self, value):
-        pass
+        if value > timezone.now().date():
+            raise serializers.ValidationError("Enter the correct date")
+        return value
 
 
 
@@ -101,24 +125,24 @@ class BorrowingSerializer(serializers.ModelSerializer):
         model = Borrowing
         fields = '__all__'
 
-    def validate_due_date(self, value):
-        pass
+    def validate(self, data):
+        if data.get("due_date") and data['due_date'] < data['borrow_date']:
+            raise serializers.ValidationError("Due date must be after borrow date")
 
-    def validate_return_date(self, value):
-        pass
+        if data.get("due_date") and data['return_date']<data['due_date']:
+            raise serializers.ValidationError("Enter the correct due date")
 
-    def validate_late_fee(self, value):
-        pass
-
+        return data
 
 
 class SearchBookSerializer(serializers.ModelSerializer):
-    author_names = serializers.SerializerMethodField()
-    category_names = serializers.SerializerMethodField()
+    author_names = serializers.SerializerMethodField(read_only=True)
+    category_names = serializers.SerializerMethodField(read_only=True)
     l_name = serializers.CharField(source= 'library.l_name',read_only= True)
     class Meta:
         model = Book
         fields = ['title','isbn','l_name','author_names','category_names']
+
     def get_author_names(self, obj):
         # returns list of first names
         return [a.first_name+" "+a.last_name for a in obj.authors.all()]
